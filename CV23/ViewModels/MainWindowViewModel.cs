a6a9558d854a79c15e3d3ef67f4d819060a7e887
NSIST96 <amelin.good@mail.ru>
2023-07-30 19:16:17 +0300

    Добавление свойства Title

--- CV23/ViewModels/MainWindowViewModel.cs
+++ CV23/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CV23.ViewModels.Base;
 
 namespace CV23.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "CV23";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
     }
 }
