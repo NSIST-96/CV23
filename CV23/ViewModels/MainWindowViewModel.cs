e72cb60b7429ff1de9d21520386875484fbd8767
NSIST96 <amelin.good@mail.ru>
2023-07-31 15:48:21 +0300

    Привязка данных к статусу программы

--- CV23/ViewModels/MainWindowViewModel.cs
+++ CV23/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,9 @@ namespace CV23.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Свойства
+
+        #region Title: string - Заголовок окна
         /// <summary>Заголовок окна</summary>
         private string _Title = "CV23";
         public string Title
@@ -11,5 +14,20 @@ namespace CV23.ViewModels
             get => _Title;
             set => Set(ref _Title, value);
         }
+        #endregion
+
+        #region Status: string - Статус программы
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готово!";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+        #endregion
+
+        #endregion
     }
 }
