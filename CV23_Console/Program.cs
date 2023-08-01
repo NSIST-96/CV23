efff4556d9f6c1c19cac1409cf5a383aec91849e
NSIST96 <amelin.good@mail.ru>
2023-08-01 19:50:52 +0300

    Извлечение данных по датам в файле

--- CV23_Console/Program.cs
+++ CV23_Console/Program.cs
@@ -1,19 +1,52 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Linq;
+using System.Globalization;
 
 namespace CV23_Console
 {
+    
     internal class Program
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-        static void Main(string[] args)
+        private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
+            var responce = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await responce.Content.ReadAsStreamAsync();
+        }
 
-            var responce = client.GetAsync(data_url).Result;
-            var csv_str = responce.Content.ReadAsStringAsync().Result;
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
 
+            while(!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+        static void Main(string[] args)
+        {
+            //var client = new HttpClient();
+
+            //var responce = client.GetAsync(data_url).Result;
+            //var csv_str = responce.Content.ReadAsStringAsync().Result;
+
+            DateTime[] dates = GetDates();
+
+            Console.WriteLine(string.Join("\r\n", dates));
 
             Console.ReadLine();
         }
