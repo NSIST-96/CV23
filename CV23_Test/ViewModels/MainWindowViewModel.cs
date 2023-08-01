1c8054d252cefc5019f1de8fed61db735e88054c
NSIST96 <amelin.good@mail.ru>
2023-08-01 16:10:13 +0300

    Доавлена базовая структура в тестовый проект

---
+++ CV23_Test/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV23_Test.ViewModels
+{
+    internal class MainWindowViewModel : Base.ViewModel
+    {
+        #region Свойства
+
+        #region Заголовок окна
+        private string _Title = "CV23_Test";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+        #endregion
+        public MainWindowViewModel() { }
+    }
+}
