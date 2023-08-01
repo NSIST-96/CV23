3f9252c5346b6545d6a03271e47270e2d72a53cd
NSIST96 <amelin.good@mail.ru>
2023-08-01 16:57:23 +0300

    Загрузка тестовой статистики

--- CV23_Console/Program.cs
+++ CV23_Console/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace CV23_Console
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace CV23_Console
 {
     internal class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+
+            var responce = client.GetAsync(data_url).Result;
+            var csv_str = responce.Content.ReadAsStringAsync().Result;
+
+
+            Console.ReadLine();
         }
     }
 }
